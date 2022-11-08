5e7481a030810d27c31f775c84deadeef5505333
anita <anita@gmail.com>
2022-11-08 10:36:49 +0700

    first commit

---
+++ app/src/main/java/com/example/salmajobs16/Activity_dialog.kt
@@ -0,0 +1,11 @@
+package com.example.salmajobs16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_dialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialog)
+    }
+}
